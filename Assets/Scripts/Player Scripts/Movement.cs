e271d561c0f21169b8341957c608b4e0cd81d8aa
AndAgGit <142849232+AndAgGit@users.noreply.github.com>
2024-02-25 20:31:07 -0800

    Basic movement development (#2)
    * Player Movement
    
    I probably should have made smaller commits along the way but uuh... woops!
    
    * Gliding Added

---
+++ Assets/Scripts/Player Scripts/Movement.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed, gravity, maxFallSpeed, jumpHeight, jumpBoost, dashSpeed, dashCooldown, dashDuration, dashMiniBoost, coyoteTime, jumpBuffer;
+    [Range(0f, 1f)]
+    public float glideEffectiveness;
+    public LayerMask layerMask;
+
+    private bool isGrounded, isDashing;
+    private float moveX, moveY, dashCD, coyoteCD, jumpBufferCD, useMaxFallSpeed;
+    private Vector2 dashDirection;
+    private Transform groundCheck;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        groundCheck = transform.Find("Ground Check").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.3f, layerMask);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCD = jumpBuffer;
+        }
+
+        if (isGrounded)
+        {
+            coyoteCD = coyoteTime;
+            moveY = -1f;
+
+            if(jumpBufferCD > 0f)
+            {
+                moveY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.Space) && !isDashing)
+            {
+                if (moveY >= 0f)
+                {
+                    moveY += jumpBoost * Time.deltaTime;
+                }
+                else if (moveY < 0f)
+                {
+                    useMaxFallSpeed = maxFallSpeed * (1f - glideEffectiveness);
+                }
+            }
+            else
+            {
+                useMaxFallSpeed = maxFallSpeed;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteCD >= 0f)
+        {
+            moveY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        if (!isDashing)
+        {
+            controller.Move(Vector2.up * moveY * Time.deltaTime);
+            
+            if(moveY >= useMaxFallSpeed)
+            {
+                moveY += gravity * Time.deltaTime;
+            }
+            else
+            {
+                moveY = useMaxFallSpeed;
+            }
+        }
+        else
+        {
+            if (moveY < 0f)
+            {
+                moveY = dashMiniBoost;
+            }
+        }
+
+
+
+
+
+
+        moveX = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCD <= 0f)
+        {
+            isDashing = true;
+            dashCD = dashCooldown;
+            dashDirection = (Vector2.right * moveX) / Mathf.Abs(moveX);
+        }
+
+        if (isDashing && dashCD >= dashCooldown - dashDuration)
+        {
+            controller.Move(dashDirection * dashSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(Vector2.right * moveX * speed * Time.deltaTime);
+        }
+
+        if(dashCD <= dashCooldown - dashDuration)
+        {
+            isDashing = false;
+        }
+
+        if(dashCD > 0f)
+        {
+            dashCD -= Time.deltaTime;
+        }
+
+        if(coyoteCD > 0f)
+        {
+            coyoteCD -= Time.deltaTime;
+        }
+
+        if (jumpBufferCD > 0f)
+        {
+            jumpBufferCD -= Time.deltaTime;
+        }
+    }
+}
