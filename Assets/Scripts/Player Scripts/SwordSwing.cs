2f2075ea587967f750f2ebed3f2ee0538d32f0c0
ColtonKor <ckorhummel@csumb.edu>
2024-04-17 17:08:38 -0700

    MS1:AttackingMechanics
    I added a sword mechanic for the player and changed the shooting mechanics so that you can aim and an arrow will show up to where you are pointing. You can also cancel the aim. The objects/scripts were added to the character prefab. I copied and removed ThrowManager from the character and added ThrowTest because I didn't want to change ThrowManager. My Scripts are in Player Scripts named ThrowTest and SwordSwing. My objects are named ArrowRotate and SwordRotate. I made a object called SideSwitch that checks where the mouse is when you swing the sword so it will be on the left or right of the character. I also added sprites for the arrow and the Bone Sword

--- Assets/Scripts/Player Scripts/SwordSwing.cs
+++ Assets/Scripts/Player Scripts/SwordSwing.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class SwordSwing : MonoBehaviour
 {
     public GameObject sword;
+    public GameObject sideSwitch;
     public float swingSpeed = 60f;
     public float cooldownDuration;
     private bool onCooldown;
     private bool swinging = false;
-    private float targetAngle = -60f;
+    private float targetAngle = -90f;
     private float speed;
+    private Camera mainCamera;
     ThrowTest swordAvailability;
 
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         swordAvailability = ThrowTest.instance;
         speed = swingSpeed * 10;
-        sword.transform.eulerAngles = new Vector3(0, 0, 60);
+        sword.transform.eulerAngles = new Vector3(0, 0, 90);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !swinging && !onCooldown && swordAvailability.ShootingState()){
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            Vector3 clickedPos = mainCamera.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, mainCamera.transform.position.z * -1f));
+            Vector3 throwDirection = new Vector3(clickedPos.x - transform.position.x, clickedPos.y - transform.position.y).normalized;
+            if(throwDirection.x < 0){
+                sideSwitch.transform.eulerAngles = new Vector3(0,-180,0);
+                sword.transform.eulerAngles = new Vector3(0, -180, 90);
+            } else {
+                sideSwitch.transform.eulerAngles = new Vector3(0,0,0);
+                sword.transform.eulerAngles = new Vector3(0, 0, 90);
+            }
             sword.SetActive(true);
             swinging = true;
             StartCoroutine(Swing());
@@ -46,7 +59,7 @@ public class SwordSwing : MonoBehaviour
             currentAngle = (currentAngle > 180) ? currentAngle - 360 : currentAngle;
         }
 
-        sword.transform.eulerAngles = new Vector3(0, 0, 60);
+        sword.transform.eulerAngles = new Vector3(0, 0, 90);
         sword.SetActive(false);
         swinging = false;
         StartCoroutine(SwordCooldown());
