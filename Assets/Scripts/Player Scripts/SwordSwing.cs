a04f5f4b69c75ea57a9f34f9a46ffded7757a59f
DARKSONIC <kevin-lehmann99@web.de>
2024-04-19 14:27:38 -0700

    Deleted unecessary scenes, reformated Code in SwordSwing.cs, added folder structure to spore scripts

--- Assets/Scripts/Player Scripts/SwordSwing.cs
+++ Assets/Scripts/Player Scripts/SwordSwing.cs
@@ -8,14 +8,13 @@ public class SwordSwing : MonoBehaviour
     public GameObject sideSwitch;
     public float swingSpeed = 60f;
     public float cooldownDuration;
-    private bool onCooldown;
+    private bool onCooldown = false;
     private bool swinging = false;
     private float targetAngle = -90f;
     private float speed;
     private Camera mainCamera;
     ThrowTest swordAvailability;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         mainCamera = Camera.main;
@@ -23,21 +22,27 @@ public class SwordSwing : MonoBehaviour
         speed = swingSpeed * 10;
         sword.transform.eulerAngles = new Vector3(0, 0, 90);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !swinging && !onCooldown && swordAvailability.ShootingState()){
+        if (Input.GetMouseButtonDown(0) && !swinging && !onCooldown && swordAvailability.ShootingState())
+        {
             Vector3 mouseScreenPosition = Input.mousePosition;
             Vector3 clickedPos = mainCamera.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, mainCamera.transform.position.z * -1f));
             Vector3 throwDirection = new Vector3(clickedPos.x - transform.position.x, clickedPos.y - transform.position.y).normalized;
-            if(throwDirection.x < 0){
+            
+            if(throwDirection.x < 0)
+            {
                 sideSwitch.transform.eulerAngles = new Vector3(0,-180,0);
                 sword.transform.eulerAngles = new Vector3(0, -180, 90);
-            } else {
+            } 
+            
+            else 
+            {
                 sideSwitch.transform.eulerAngles = new Vector3(0,0,0);
                 sword.transform.eulerAngles = new Vector3(0, 0, 90);
             }
+            
             sword.SetActive(true);
             swinging = true;
             StartCoroutine(Swing());
@@ -49,7 +54,8 @@ public class SwordSwing : MonoBehaviour
         float currentAngle = sword.transform.eulerAngles.z;
         currentAngle = (currentAngle > 180) ? currentAngle - 360 : currentAngle;
 
-        while (currentAngle > targetAngle){
+        while (currentAngle > targetAngle)
+        {
             float step = speed * Time.deltaTime;
             sword.transform.Rotate(0, 0, -step);
             currentAngle -= step;
