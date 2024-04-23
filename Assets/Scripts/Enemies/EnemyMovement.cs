af656cf81e1c872734fdfad6327484cd2ac7e3cf
rumkkee <accesareo@gmail.com>
2024-04-23 08:42:43 -0700

    Created EnemyMovement class for base movement behavior
    This class handles movement behaviors that all enemies should have.

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    protected Rigidbody rb;
+    [SerializeField] private float knockbackForce;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void TakeKnockback(Vector2 direction)
+    {
+        Vector2 normalized = direction.normalized;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(normalized * knockbackForce, ForceMode.Impulse);
+    }
+}
