8d966395094431f09dfd74751b364be3dc755a79
rumkkee <accesareo@gmail.com>
2024-04-22 21:40:31 -0700

    Flying enemy floats towards the player's direction

---
+++ Assets/Scripts/Enemies/EnemyFlyingMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyingMovement : MonoBehaviour
+{
+    public GameObject target;
+    private Rigidbody rb;
+
+    [SerializeField] private float chaseSpeed;
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {  
+        StartCoroutine(FollowPlayer());  
+    }
+
+    public IEnumerator FollowPlayer()
+    {
+        while (true)
+        {
+            Vector2 moveDirection = target.transform.position - transform.position;
+            rb.AddForce(moveDirection * chaseSpeed, ForceMode.Force);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
