b36f4cd3ade7bcc3ff997e24e408750450158774
rumkkee <accesareo@gmail.com>
2024-04-30 19:13:07 -0700

    Implemented the SporeTrajectoryRenderer script
    This script draws a lineRenderer to represent the spore's trajectory, accounting for the throw speed and gravity.

---
+++ Assets/Scripts/Player Scripts/SporeTrajectoryRenderer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporeTrajectoryRenderer : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private ThrowTest throwTest;
+
+    public int numPoints = 10;
+    public float timeStep = 0.1f;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        throwTest = GetComponentInParent<ThrowTest>();
+    }
+
+    private void OnEnable()
+    {
+        lineRenderer.positionCount = numPoints;
+    }
+
+    void Update()
+    {
+        // access the throw direction and force
+        float throwForce = throwTest.throwForce;
+        Vector3 direction = throwTest.direction.normalized;
+        direction = new Vector3(direction.y, -direction.x, 0);
+
+        Vector3 launchVelocity = direction * throwForce;
+
+        Vector3 position = Vector3.zero;
+        for(int i = 0; i < numPoints; i++)
+        {
+            lineRenderer.SetPosition(i, position);
+            position += launchVelocity * timeStep;
+            launchVelocity += Physics.gravity * timeStep;
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        lineRenderer.positionCount = 0;
+    }
+}
