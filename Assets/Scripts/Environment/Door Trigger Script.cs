a1493e4ecdaa4fa86a2f9591f73e4a5c8e98f33d
Kyle Lynn <lynninlcn@gmail.com>
2024-04-30 18:41:47 -0700

    Fixed teleport to exit object
    Updated transition script to transition to exit object. You need to specify which room and what exit object ie exit1, exit2, etc. the player will spawn in.

--- Assets/Scripts/Environment/Door Trigger Script.cs
+++ Assets/Scripts/Environment/Door Trigger Script.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class DoorTriggerScript : MonoBehaviour
 {
-
+    
     public int nextRoomIndex;
+    public string destinationExitName;
     public NewBehaviourScript worldManager; 
     
     
     // Start is called before the first frame update
     void Start()
     {
+        
         worldManager = FindObjectOfType<NewBehaviourScript>();
         if (worldManager == null)
         {
             Debug.Log("sorry there bud, room manager not found!");
         }
+        
     }
 
     
@@ -24,8 +27,9 @@ public class DoorTriggerScript : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            worldManager.TransitionToRoomLogic(nextRoomIndex, transform.position); 
-            Debug.Log("entered door");
+            Debug.Log("entered door " + destinationExitName);
+            worldManager.TransitionToRoomLogic(nextRoomIndex, destinationExitName); 
+            
         }
     }
 }
--- Assets/Scripts/Environment/Room Management Script.cs
+++ Assets/Scripts/Environment/Room Management Script.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
 
     public GameObject[] rooms; //this array holds references to all room prefabs
+    public GameObject player; 
     private GameObject currentRoom; //this is the current active room 
+    private CharacterController playerCont; //player's character controller which will be deactivated then reactivated
     
     
     // Start is called before the first frame update
     void Start()
     {
+        //get access to the player's character controller  
+        playerCont = player.GetComponent<CharacterController>(); 
+        
+        if (playerCont != null)
+        {
+            playerCont.enabled = true; // if exists, make sure it starts out as enabled 
+        }
+        else
+        {
+            Debug.LogError("CharacterController component not found on playerObject.");
+            return;
+        }
+        
+        
         if (rooms.Length > 0)
         {
             currentRoom = Instantiate(rooms[0], Vector3.zero, Quaternion.identity); 
@@ -21,6 +38,7 @@ public class NewBehaviourScript : MonoBehaviour
         {
             Debug.Log("no room prefabs in the rooms array!");
         }
+        
        
     }
 
@@ -32,34 +50,53 @@ public class NewBehaviourScript : MonoBehaviour
     
     //this method will be called when a player enters a trigger
     //specify the index in the specific trigger/door itself 
-    public void TransitionToRoomLogic(int roomIndex, Vector3 playerSpawnPosition )
+    public void TransitionToRoomLogic(int roomIndex, string exitName )
     {
+        Debug.Log("Entering Room"+ roomIndex + "through door " + exitName);
+        //transition logic 
+        
         //deactivate current room if it already exists 
         if (currentRoom != null)
         {
+            Debug.Log("destroying current room");
             Destroy(currentRoom);
         }
-        
         
         //check to see if the requested room index is within the bounds of the array
         if (roomIndex >= 0 && roomIndex < rooms.Length)
         {
-            //instantiate new room, set to current room, set propwer player position 
+            //instantiate new room, set to current room, set proper player position 
             currentRoom = Instantiate(rooms[roomIndex], Vector3.zero, Quaternion.identity); 
-            MovePlayerToSpawnPosition(playerSpawnPosition);
+            Debug.Log("creating room" + roomIndex);
         }
         else
         {
             Debug.Log("Sorry, bud. You don't have a valid index there.");
         }
         
-    }
+        //room instantiated, now spawn player at exit door:
 
-
-    private void MovePlayerToSpawnPosition(Vector3 spawnPosition)
-    {
+        //find the object whose position we will spawn at within the new room
+        GameObject destinationObject = GameObject.Find(exitName);
+        Debug.Log("destination object is" + destinationObject);
+        if (destinationObject != null)
+        {
+            playerCont.enabled = false;
+            Vector3 playerSpawnPosition = destinationObject.transform.position;
+            player.transform.position = playerSpawnPosition; 
+            Debug.Log("spawning player at " + playerSpawnPosition);
+            playerCont.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Sorry bud, your destination object not found in the new room!");
+        }
         
+
+
+
+
     }
-    
+
     
 }
