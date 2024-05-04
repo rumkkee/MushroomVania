06041aff0a006f058e0c54e9b05aa8ee1478f7d1
rumkkee <accesareo@gmail.com>
2024-05-04 11:25:33 -0700

    Set up VenomAttackManager

---
+++ Assets/Scripts/Enemies/VenomAttackManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VenomAttackManager : MonoBehaviour
+{
+    [SerializeField] private VenomBall venomBallPrefab;
+
+    private void Start()
+    {
+        VenomBallVolley();
+    }
+
+    // Shoots 5 balls of venom
+    private void VenomBallVolley()
+    {
+
+    }
+
+}
