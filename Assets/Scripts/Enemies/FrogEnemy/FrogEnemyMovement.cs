05e9f40ccc6f2b11d3dfae2bd611d6cd2ee3aa5c
ColtonKor <ckorhummel@csumb.edu>
2024-05-04 13:31:54 -0700

    Added Inheritance to some enemies

--- Assets/Scripts/Enemies/FrogEnemy/FrogEnemyMovement.cs
+++ Assets/Scripts/Enemies/FrogEnemy/FrogEnemyMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogEnemyMovement : MonoBehaviour
+public class FrogEnemyMovement : EnemyMovement
 {
     // public bool facingRight = false;
     public bool isGrounded = false;
