39453bf7b778d3466d8c2d8a2d3a1465c23e4b49
DARKSONIC <kevin-lehmann99@web.de>
2024-05-05 14:48:56 -0700

    Implemented Boss level
    - Press P to reveal plattform for 10 seconds

---
+++ Assets/PlatformEnabler.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformEnabler : MonoBehaviour
+{
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    void Start()
+    {
+        platform1.SetActive(false);
+        platform2.SetActive(false);
+        platform3.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            EnablePlatforms();
+        }
+    }
+
+    IEnumerator EnablePlatformTimer()
+    {
+        platform1.SetActive(true);
+        platform2.SetActive(true);
+        platform3.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        platform1.SetActive(false);
+        platform2.SetActive(false);
+        platform3.SetActive(false);
+    }
+
+    public void EnablePlatforms()
+    {
+        StartCoroutine(EnablePlatformTimer());
+    }
+}
