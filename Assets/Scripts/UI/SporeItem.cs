d7e6b3b0b5b331cd89edb6eff083761aaa93fa0a
rumkkee <accesareo@gmail.com>
2024-05-05 20:17:02 -0700

    Set up Charge Delay and Charge coroutines for spore items

--- Assets/Scripts/UI/SporeItem.cs
+++ Assets/Scripts/UI/SporeItem.cs
@@ -19,4 +19,51 @@ public class SporeItem : MonoBehaviour
 
     public AudioClip SelectSound;
 
+    public float maxCharge = 100f;
+    public float currentCharge;
+
+    public float chargeTakenPerUse;
+    public float chargeDelay;
+    public float rechargeRatePerSec;
+    
+
+    private void Start()
+    {
+        if(currentCharge < maxCharge)
+        {
+            Debug.Log("Starting charge from: " + gameObject.name);
+            StartCoroutine(CheckCharge());
+        }
+    }
+
+    private IEnumerator CheckCharge()
+    {
+        if(currentCharge < maxCharge)
+        {
+            yield return StartCoroutine(ChargeDelay());
+            yield return StartCoroutine(Charge());
+        }
+    }
+
+    private IEnumerator ChargeDelay()
+    {
+        Debug.Log("Entered ChargeDelay");
+        float timeRemaining = chargeDelay;
+        do
+        {
+            timeRemaining -= Time.deltaTime;
+            yield return null;
+        } while (timeRemaining > 0);
+    }
+
+    private IEnumerator Charge()
+    {
+        Debug.Log("Entered Charge");
+        do
+        {
+            currentCharge += rechargeRatePerSec * Time.deltaTime;
+            yield return null;
+        } while (currentCharge < maxCharge);
+        currentCharge = maxCharge;
+    }
 }
