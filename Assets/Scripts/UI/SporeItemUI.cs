d5fd17ee7885a04de2344d8b68d3339827787c5a
rumkkee <accesareo@gmail.com>
2024-05-08 12:41:22 -0700

    Implemented the ChargeBar for a single spore

---
+++ Assets/Scripts/UI/SporeItemUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SporeItemUI : MonoBehaviour
+{
+    [SerializeField] private Slider chargebar;
+    public Image sliderImageSlot;
+    [SerializeField] private SporeItem sporeItem;
+
+    private void Awake()
+    {
+        // Have sporeItem be assigned on Awake from the SporeUIManager
+
+        sliderImageSlot = chargebar.GetComponentInChildren<Image>();
+        sliderImageSlot.sprite = sporeItem.sporeSprite;
+    }
+
+    private void Update()
+    {
+        SetCharge();
+    }
+
+    public void SetSporeItem(SporeItem spore)
+    {
+        sporeItem = spore;
+    }
+
+    public void SetCharge()
+    {
+        float newValue = sporeItem.maxCharge - sporeItem.currentCharge;
+        chargebar.value = newValue;
+    }
+
+}
