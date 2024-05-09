b1c65c37231be4ea64adbbaadaecad88dc9eadb1
ColtonKor <ckorhummel@csumb.edu>
2024-05-09 11:31:56 -0700

    Added glide animation to player

--- Assets/AnimationScript.cs
+++ Assets/AnimationScript.cs
@@ -6,6 +6,7 @@ public class AnimationScript : MonoBehaviour
 {
     private Animator ar;
     private SpriteRenderer sr;
+    private bool grounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +17,34 @@ public class AnimationScript : MonoBehaviour
 
     void Update()
     {
+        Vector3 startPoint = transform.position;
+        float halfHeight = .6f;
+        RaycastHit hit;
+        // Debug.DrawRay(startPoint, Vector3.down * halfHeight, Color.red);
+        if (Physics.Raycast(startPoint, Vector3.down, out hit, halfHeight))
+        {
+            if (hit.collider.isTrigger){return;}
+            grounded = true;
+        } else {
+            grounded = false;
+        }
+
+
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
             sr.flipX = true;
         }
 
         if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
             sr.flipX = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || grounded){
+            ar.SetBool("Glide", false);
+            ar.SetTrigger("FinishGlide");
+        }
+
+        if (Input.GetKey(KeyCode.W) && !grounded){
+            ar.SetBool("Glide", true);
         }
         
     }
