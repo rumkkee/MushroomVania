1c0e107db11ab481016e7b4351a69c6ba816812b
jgarn6733 <143364247+jgarn6733@users.noreply.github.com>
2024-05-08 19:27:55 -0700

    Fixed the boss music

--- Assets/Scripts/LevelMusic.cs
+++ Assets/Scripts/LevelMusic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class LevelMusic : MonoBehaviour
@@ -28,7 +29,12 @@ public class LevelMusic : MonoBehaviour
             {
                 levelMusic.Stop();
             }
-            bossMusic.Play();
+
+            if (!bossMusic.isPlaying)
+            {
+                bossMusic.Play();
+            }
+            
         }
         else
         {
